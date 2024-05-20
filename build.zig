082631fddd1fd4d2e3b9cde08683f3497781cd8a
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-20 02:03:45 +0100

    feat: update main.cpp path

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{ .name = "main", .target = target, .optimize = optimize });
-    exe.addCSourceFile(.{ .file = .{ .path = "main.cpp" }, .flags = &.{"-std=c++14"} });
+    exe.addCSourceFile(.{ .file = .{ .path = "src/main.cpp" }, .flags = &.{"-std=c++14"} });
     exe.linkSystemLibrary("c++");
     exe.linkLibCpp();
     b.installArtifact(exe);
