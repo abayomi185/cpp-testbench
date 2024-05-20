23ec09b3a20385ca4fed18681d79d18acdc38367
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-20 02:01:39 +0100

    feat: add build.zig

---
+++ build.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const exe = b.addExecutable(.{ .name = "main", .target = target, .optimize = optimize });
+    exe.addCSourceFile(.{ .file = .{ .path = "main.cpp" }, .flags = &.{"-std=c++14"} });
+    exe.linkSystemLibrary("c++");
+    exe.linkLibCpp();
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
